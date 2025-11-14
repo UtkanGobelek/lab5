4f7e19ae9d1e28ddeb406144e0648b3d113ee451
UtkanGobelek <gumututkan@gmail.com>
2025-11-14 12:28:35 +0300

    shortened code

--- src/sec2.java
+++ src/sec2.java
@@ -10,8 +10,7 @@ public class sec2 {
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter two numbers, the first being the base and the second being the power");
-        System.out.println("enter first number");
+        System.out.println("enter base");
         long num1 = sc.nextLong();
         System.out.println("enter second number");
         int num2 = sc.nextInt();
